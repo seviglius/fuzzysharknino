44292d97efb7a2e0b1e56cd96d14163253dec667
mdebauge <mdebau@gmail.com>
2016-04-28 13:19:12 -0700

    wp head and footer

--- front-page.php
+++ front-page.php
@@ -56,6 +56,7 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.9.24/paper-full.min.js"></script>
   <script type="text/paperscript" src="<?php bloginfo('template_url'); ?>/js/sccawaves.js" canvas="myCanvas"></script>
+  <?php wp_head(); ?>
 </head>
 
 <body>
@@ -223,6 +224,7 @@
     </div> -->
   </div>
 
+<?php wp_footer(); ?>
 
 <!-- Scripts
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
