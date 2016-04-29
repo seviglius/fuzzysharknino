aa5c9f5ba3aa5502d2e77329c0e62c63c4279ffa
mdebauge <mdebau@gmail.com>
2016-04-28 17:23:46 -0700

    try again

--- js/ie.js
+++ js/ie.js
@@ -1,21 +1,68 @@
-function GetIEVersion() {
-  var sAgent = window.navigator.userAgent;
-  var Idx = sAgent.indexOf("MSIE");
+/**
+  Target IE 10 with JavaScript and CSS property detection.
+  
+  # 2013 by Tim Pietrusky
+  # timpietrusky.com
+ **/
 
-  // If IE, return version number.
-  if (Idx > 0) 
-    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+ // IE 10 only CSS properties
+ var ie10Styles = [
+     'msTouchAction',
+     'msWrapFlow',
+     'msWrapMargin',
+     'msWrapThrough',
+     'msOverflowStyle',
+     'msScrollChaining',
+     'msScrollLimit',
+     'msScrollLimitXMin',
+     'msScrollLimitYMin',
+     'msScrollLimitXMax',
+     'msScrollLimitYMax',
+     'msScrollRails',
+     'msScrollSnapPointsX',
+     'msScrollSnapPointsY',
+     'msScrollSnapType',
+     'msScrollSnapX',
+     'msScrollSnapY',
+     'msScrollTranslation',
+     'msFlexbox',
+     'msFlex',
+     'msFlexOrder'];
 
-  // If IE 11 then look for Updated user agent string.
-  else if (!!navigator.userAgent.match(/Trident\/7\./)) 
-    return 11;
+ var ie11Styles = [
+     'msTextCombineHorizontal'];
 
-  else
-    return 0; //It is not IE
-}
+ /*
+  * Test all IE only CSS properties
+  */
+ var d = document;
+ var b = d.body;
+ var s = b.style;
+ var ieVersion = null;
+ var property;
 
-if (GetIEVersion() > 0) 
-   alert("This is IE " + GetIEVersion());
-$( ".wave-wrap" ).hide();
-else 
-   alert("This is not IE.");
+ // Test IE10 properties
+ for (var i = 0; i < ie10Styles.length; i++) {
+     property = ie10Styles[i];
+
+     if (s[property] != undefined) {
+         ieVersion = "ie10";
+         createEl("IE10 style found: " + property);
+     }
+ }
+
+ // Test IE11 properties
+ for (var i = 0; i < ie11Styles.length; i++) {
+     property = ie11Styles[i];
+
+     if (s[property] != undefined) {
+         ieVersion = "ie11";
+         createEl("IE11 style found: " + property);
+     }
+ }
+
+ if (ieVersion) {
+     $( ".wave-wrap" ).hide();
+ } else {
+     createEl('Not IE10 or 11.');
+ }
